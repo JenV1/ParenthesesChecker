0ee91993a6d575b4a1c0b00d60dc4c96522d85cc
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-04-28 16:18:02 +0100

    parenthesesChecker passes tests

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,8 +1,37 @@
+import java.util.Stack;
+
 public class ParenthesesChecker {
+
     public ParenthesesChecker() {
     }
 
     public boolean checkParentheses(String string) {
-        return false;
+
+        Stack<String> parenthesesHolder = new Stack<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '(') {
+                parenthesesHolder.push("(");
+            } else if (string.charAt(i) == ')') {
+                parenthesesHolder.push(")");
+            }
+        }
+
+        int fronts = 0;
+        int backs = 0;
+        int beginningSize = parenthesesHolder.size();
+
+        for (int i = 0; i < beginningSize; i++) {
+            if (parenthesesHolder.peek().equals(")")) {
+                parenthesesHolder.pop();
+                backs++;
+            } else if (parenthesesHolder.pop().equals("(") && backs >= fronts) {
+                fronts++;
+            }
+        }
+
+        return (parenthesesHolder.isEmpty() && (fronts == backs));
     }
+
+
 }
