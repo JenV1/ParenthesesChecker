ecbc85604cc77799d3f182dc648bf9b81528f394
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-04-29 10:13:23 +0100

    added comment outlining algorithm

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -17,6 +17,20 @@ public class ParenthesesChecker {
         Collections.addAll(backs, ")",">","}","]");
         int frontsInARow = 0;
 
+        /*The algorithm is as follows:
+            go through the string character by character,
+            if it's a bracket, add it to the stack.
+            Keep count of the number of bracket fronts in a row.
+            If we have a back bracket, we delete it and see
+            if it matches the front one before it,
+            if there is nothing before it or it's a different bracket type,
+            the string fails the test.
+            If it does match the one before,
+            we take away the matching front bracket as well and minus one
+            from frontsInARow.
+            At the end we make sure frontsInARow is 0, so that each bracket
+            has a back and not just a start.
+        */
         for (int i = 0; i < string.length(); i++) {
             if (possibleElements.contains(string.substring(i,i+1))) {
                 parenthesesHolder.push(string.substring(i, i + 1));
