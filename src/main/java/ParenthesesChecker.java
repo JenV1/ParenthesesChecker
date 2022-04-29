bc5eba6037a232c4a15835bcb2f4fcd5591cc6b6
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-04-29 16:36:04 +0100

    a slightly simplified version

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -15,43 +15,37 @@ public class ParenthesesChecker {
         Collections.addAll(possibleElements, "(",")","[","]","{","}","<",">");
         Collections.addAll(fronts, "(","<","{","[");
         Collections.addAll(backs, ")",">","}","]");
-        int frontsInARow = 0;
 
         /*The algorithm is as follows:
             go through the string character by character,
-            if it's a bracket, add it to the stack.
-            Keep count of the number of bracket fronts in a row.
-            If we have a back bracket, we delete it and see
+            if it's a front bracket, add it to the stack.
+            If we have a back bracket, we see
             if it matches the front one before it,
             if there is nothing before it or it's a different bracket type,
             the string fails the test.
             If it does match the one before,
-            we take away the matching front bracket as well and minus one
-            from frontsInARow.
-            At the end we make sure frontsInARow is 0, so that each bracket
+            we take away the matching front bracket.
+            At the end we make sure the stack is empty, so that each bracket
             has a back and not just a start.
         */
+        
         for (int i = 0; i < string.length(); i++) {
-            if (possibleElements.contains(string.substring(i,i+1))) {
+            if (fronts.contains(string.substring(i,i+1))) {
                 parenthesesHolder.push(string.substring(i, i + 1));
-                if (fronts.contains(string.substring(i,i+1))) {
-                    frontsInARow++;
+            } else if (backs.contains(string.substring(i,i+1))){
+                int currentElement = possibleElements.indexOf(string.substring(i, i + 1));
+                if (parenthesesHolder.isEmpty()){
+                    return false;
+                }
+                else if (parenthesesHolder.peek().equals(possibleElements.get(currentElement-1))) {
+                    parenthesesHolder.pop();
                 } else {
-                    int currentElement = possibleElements.indexOf(parenthesesHolder.pop());
-                    if (parenthesesHolder.isEmpty()){
-                        return false;
-                    }
-                    else if (parenthesesHolder.peek().equals(possibleElements.get(currentElement-1))) {
-                        parenthesesHolder.pop();
-                        frontsInARow--;
-                    } else {
-                        return false;
-                    }
+                    return false;
                 }
             }
         }
 
-        return frontsInARow == 0;
+        return parenthesesHolder.isEmpty();
 
 
 
